dcce070021245453e8daa469664e8fc6908a0255
송영창 <sych7389@dgu.ac.kr>
2022-11-05 00:29:48 +0900

    [feature] 로그인, 회원가입, 로그아웃 구현

---
+++ backend/index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/dev');
+const { auth } = require("./middleware/auth");
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+app.post('/api/users/login', (req, res) => {
+  // 데이터베이스에서 이메일 찾기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "존재하지 않는 유저입니다."
+      })
+    }
+
+    // 비밀번호 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+      // 비밀번호 일치하면 토큰 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 쿠키에 저장
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
+    })
+  })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true, // role 0이면 일반유저, 아니면 admin
+    name: req.user.name,
+    email: req.user.email,
+    role: req.user.role
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" },
+    (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
