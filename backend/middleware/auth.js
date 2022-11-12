49b0d7a9bf3881765993f80b256cfb6b7c6af64f
KimKongDol <als6213@naver.com>
2022-11-13 06:26:09 +0900

    모든 파일 주석처리

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,16 +1,23 @@
-const { User } = require('../models/User');
+//모델에서 유저를 가지고 온다.
+const { User } = require('../models/User'); 
 
+//권한 인증처리를 하는 곳
 let auth = (req, res, next) => {
-    // 쿠키에서 토큰 가져와 복호화해 유저 찾기
+    
+    // 클라이언트 쿠키에서 토큰을 가져오고 
     let token = req.cookies.x_auth;
+    // 복호화해 유저를 찾는다
     User.findByToken(token, (err, user) => {
         if(err) throw err;
+        //유저가 없을 때
         if(!user) return res.json({isAuth: false, error: true})
-
+        //유저가 있을 때 리퀘스트에 정보 다 넣어줌
         req.token = token;
         req.user = user;
+        //미들웨어 다음으로 findbyToken(index.js)으로 감
         next();
     })
 }
 
+//타 파일에서도 auth쓸 수 있게 내보냄
 module.exports = { auth };
--- backend/models/User.js
+++ backend/models/User.js
@@ -29,6 +29,9 @@ const userSchema = mongoose.Schema({
     }
 })
 
+
+//비밀번호를 암호화한 후 save로 넘길거임
+//user모델에 'save'하기 전(pre)에 할 동장
 userSchema.pre('save', function (next) {
     var user = this;
 
@@ -36,8 +39,10 @@ userSchema.pre('save', function (next) {
         // 비밀번호 암호화
         bcrypt.genSalt(saltRounds, function (err, salt) {
             if (err) return next(err)
+            //user.password는 순수한 비밀번호, hash가 암호화된 비밀번호
             bcrypt.hash(user.password, salt, function (err, hash) {
                 if (err) return next(err)
+                //성공하면 hash로 비밀번호를 바꿔줌
                 user.password = hash
                 next()
             })
@@ -52,34 +57,39 @@ userSchema.methods.comparePassword = function (plainPassword, callback) {
     // 입력받은 비밀번호를 암호화해 저장된 비밀번호와 비교
     bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
         if (err) return callback(err);
+        //맞으면 트루값을 리턴        
         callback(null, isMatch)
     })
 }
 
+//jsonwebtoken을 이용해서 비밀번호를 비교하고 저장된 것과 맞는지 비교한다.
 userSchema.methods.generateToken = function (callback) {
-
     var user = this;
-    // jsonwebtoken을 이용해 토큰 생성
+    //db에 있는 아이디를 말한다.
+    //'secretToeken'을 넣으면 user_id가 나온다. 토큰으로 유저를 알 수 있다.
     var token = jwt.sign(user._id.toHexString(), 'secretToken')
     user.token = token
     user.save(function (err, user) {
         if (err) return callback(err);
+         //에러없이 세이브됐으면 유저 정보만 보내준다.
         callback(null, user)
     })
 }
 
+//토큰을 가져와서 decode하는 부분(auth관련)
 userSchema.statics.findByToken = function(token, callback) {
     var user = this;
-
     // 토큰 decode
     jwt.verify(token, 'secretToken', function(err, decoded) {
+        //유저아이디로 유저찾고 클라이언트에서 가져온 토큰과 DB토큰이 일치하는지 확인
         user.findOne({ "_id": decoded, "token": token }, function(err, user){
             if(err) return callback(err);
+            //에러없으면 유저정보 줌
             callback(null, user)
         })
     })
 }
-
+//스키마는 모델이 감싸야한다.
 const User = mongoose.model('User', userSchema)
-
+//다른 파일에서도 모듈을 쓸 수 있게
 module.exports = { User }
