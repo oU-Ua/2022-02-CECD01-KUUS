e1ce54dcb24038ee3fa12598bc633d18b11b750f
송영창 <sych7389@dgu.ac.kr>
2022-11-18 23:13:53 +0900

    [refactor] users 라우터 분리

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 //비밀키를 가져올 변수
 const config = require('./config/dev');
-const { auth } = require("./middleware/auth");
-//유저 모델스키마를  가져오겠다
-const { User } = require('./models/User');
 //AirLabsAPI 호출 함수
 const { callAirLabs } = require("./middleware/airlabs");
 //AirLabsAPI 호출에 사용되는 매개변수
@@ -20,6 +17,7 @@ const params = {
   _fields: "dep_iata, dep_time, arr_iata, arr_time"
 }
 
+const usersRouter = require('./routes/users')
 const myPageRouter = require('./routes/myPage')
 
 //application/x-www-form-urlencodec를 분석해서 가져오게 해줌
@@ -42,54 +40,6 @@ app.get('/', (req, res) => {
   res.send('백이 연결되었습니다')
 })
 
-//회원가입할 때 필요한 정보들을 client에서 가져오면 DB에 담아줌
-app.post('/api/users/register', (req, res) => {
-  //req.body에는 {name:"kim" id:"~~"}이런걸 담고 있음
-  const user = new User(req.body)
-
-  //mongoDB에 담게해주는 부분
-  user.save((err, userInfo) => {
-    //에러나 성공메세지모두 json형식으로 반환할 예정
-    if (err) return res.json({ success: false, err })
-    return res.status(200).json({
-      success: true
-    })
-  })
-})
-
-//요청된 이메일을 데이터 베이스에서 찾고,
-//이메일이 있다면 비밀번호가 맞는지 찾는다.
-//비밀번호가 맞다면 토큰을 생성한다.
-app.post('/api/users/login', (req, res) => {
-  // 데이터베이스에서 이메일 찾기
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (!user) {
-      return res.json({
-        loginSuccess: false,
-        message: "존재하지 않는 유저입니다."
-      })
-    }
-
-    // 비밀번호 확인
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch)
-        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
-
-      // 비밀번호 일치하면 토큰 생성
-      user.generateToken((err, user) => {
-        //statue400은 에러가 있다는 의미고, 클라이언트한테 리턴해줌
-        if (err) return res.status(400).send(err);
-
-        //토큰을 저장한다. 쿠키에 저장할 예정. 개발자창에서 쿠키 확인 가능
-        //개발자창에서 name:x_auth, value:khsdfhkfhsmdfdk 이런식으로 확인 가능
-        res.cookie("x_auth", user.token)
-          .status(200)
-          .json({ loginSuccess: true, userId: user._id })
-      })
-    })
-  })
-})
-
 //사용자에게서 항공편명, 출발지를 입력받아 api로 비행일정 검색
 //지역이나 공항이름을 입력받아 iata 코드로 변환하고자 하지만 우선 iata코드 입력하도록 구현
 app.post('/api/schedules/find', (req, res) => {
@@ -108,35 +58,7 @@ app.post('/api/schedules/find', (req, res) => {
   })
 })
 
-
-//auth는 미들웨어로 리퀘스트받고 auth인지 확인하는 부분
-//middleware인 auth.js를 통과해야 실행됨
-app.get('/api/users/auth', auth, (req, res) => {
-  res.status(200).json({
-    //auth.js에서 user를 가져왔기 때문에 id를 쓸 수 있음
-    _id: req.user._id,
-    // role 0이면 일반유저, 아니면 admin
-    isAdmin: req.user.role === 0 ? false : true,
-    name: req.user.name,
-    email: req.user.email,
-    role: req.user.role
-  })
-})
-
-
-//로그아웃하는 부분
-app.get('/api/users/logout', auth, (req, res) => {
-  //DB에서 로그아웃하려는 유저를 찾아서 업데이트
-  User.findOneAndUpdate({ _id: req.user._id },
-    //찾은 유저의 토큰을 지워줌
-    { token: "" },
-    (err, user) => {
-      if (err) return res.json({ success: false, err });
-      return res.status(200).send({
-        success: true
-      })
-    })
-})
+app.use('/api/users', usersRouter)
 
 //$run start해서 제대로 실행이 되면 콘솔에서 포트 출력
 app.listen(port, () => {
