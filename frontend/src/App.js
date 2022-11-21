2930b3e89cd8bfc7915473f98c5445716f2b9ac5
KimKongDol <als6213@naver.com>
2022-11-21 11:15:54 +0900

    [feat]mypage생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import LandingPage from './components/views/LandingPage/LandingPage.js';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
 import FlightSearchPage from './components/views/FlightSearchPage/FlightSearchPage';
+import MyPage from './components/views/MyPage/MyPage';
 import Auth from './hoc/auth';
 import Header from './components/views/header/header';
 import Footer from './components/views/footer/footer';
@@ -26,6 +27,8 @@ function App() {
             <Route exact path="/login" component={Auth(LoginPage, false) } />
             <Route exact path="/register" component={Auth(RegisterPage, false)} />
             <Route exact path="/flightsearch" component={Auth(FlightSearchPage, null )  } />
+            {/* mypage다 만들고 접근을 true로 바꾸기 */}
+            <Route exact path="/mypage" component={Auth(MyPage, null) } />
           </Switch>
         </div>
       </Router>
