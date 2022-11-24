09e6d3a5abb1764d806ca0596172096c3dc05614
송영창 <sych7389@dgu.ac.kr>
2022-11-24 13:59:24 +0900

    [feat] 출발일에 대한 처리 추가

--- backend/routes/flightsearch.js
+++ backend/routes/flightsearch.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const bodyParser = require("body-parser");
+const { DateTime } = require('luxon')
 const { Airport } = require('../models/Airports');
 router.use(bodyParser.json());
 
-// req 입력값: 출발지, 도착지, 항공편명
+// req 입력값: departure, arrival, flight_iata, date(YYYY-MM-DD), time(HH:MM)
 router.get('/', (req, res) => {
     Airport.findByNames(req.body.departure, req.body.arrival, (err, results)=>{
         if(err){
@@ -13,10 +14,18 @@ router.get('/', (req, res) => {
         }
         else {
             //console.log(results);
+            // 사용자에게 입력받은 출발일, 출발시간 처리부분
+            // flightaware에서 요구하는 형식으로 변환해 저장
+            var daytime = DateTime.fromISO(req.body.date + "T" + req.body.time+":00");
+            daytime.numberingSystem
+            var time_kr = daytime.toISO({suppressMilliseconds: true})
+            var time_utc = daytime.toUTC().toISO({suppressMilliseconds: true})
             // 출력 및 db 저장을 위한 json 객체틀
             var json = {
                 departure: "",
                 dep_iata:"",
+                dep_time_utc: time_utc,
+                dep_time_kr: time_kr,
                 arrival:"",
                 arr_iata:"",
                 Flight_No: req.body.flight_iata
@@ -33,8 +42,9 @@ router.get('/', (req, res) => {
                     json.arr_iata = result.iata_code
                 }
             });
-            res.status(200).send(json);
+            res.status(200).send(json)
         }
     })
 })
+
 module.exports = router
