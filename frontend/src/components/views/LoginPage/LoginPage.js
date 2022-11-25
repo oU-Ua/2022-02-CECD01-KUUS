7c8ae6f329a5b0a862f0473bba1c4dc20f63ee5d
KimKongDol <als6213@naver.com>
2022-11-25 16:31:11 +0900

    [fix]로그인 페이지 고침

--- frontend/src/components/views/LoginPage/LoginPage.js
+++ frontend/src/components/views/LoginPage/LoginPage.js
@@ -40,7 +40,8 @@ function LoginPage(props) {
 
 
     return (
-            <div className="static-slider-head">
+        <div>
+            <div className="static-slider-head-top static-slider-head">
                 <div className="" id="forms-component">
                     <Container>
                         <Row className="justify-content-center">
@@ -49,45 +50,47 @@ function LoginPage(props) {
                             </Col>
                         </Row>
                     </Container>
-                </div>                 
-                <Container>
-                    <Row className="justify-content-center">
-                        <Col className="align-self-center text-center">
-                            <div style={{maxWidth:'50vw'}}>                            
-                                <Container>
-                                    <Row>
-                                        <Col md="12">
-                                            <form className="row" onSubmit={onSubmitHandler} >
-                                                <FormGroup className="col-md-6" >
-                                                    <label>Email</label>
-                                                    <input type="email" className="form-control" id="email" placeholder="Enter email" value={Email} onChange={onEmailHandler} />
-                                                    <label>Password</label>
-                                                    <input type="password" className="form-control" id="password" placeholder="Password" value={Password} onChange={onPasswordHandler} />
-                                                    <br />
-                                                    <Col md="12">
-                                                        <button type="submit"  className="btn btn-success waves-effect waves-light m-r-10">
-                                                            로그인
-                                                        </button>                                                        
-                                                    </Col>
-                                                </FormGroup>   
-                                                <div className="col-md-6">
-                                                    <h4 style={{color: 'white', margin:'60px 0'}} className="">회원이 아니신가요?</h4>
-                                                    <Link to="/register">   
-                                                        <button type="submit"  className="btn btn-success waves-effect waves-light m-r-10">
-                                                        회원가입 하기                                                          
-                                                        </button>   
-                                                    </Link>                                         
-                                                </div>                                             
-                                            </form>
-                                            
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>            
+                </div>       
             </div>
+            <div className='spacer'>
+                    <Container>
+                        <Row className="justify-content-center">
+                            <Col className=" text-center">                          
+                                    <Container>
+                                        <Row>
+                                            <Col md="12">
+                                                <form className="row" onSubmit={onSubmitHandler} >
+                                                    <FormGroup className="col-md-6" >
+                                                        <label>Email</label>
+                                                        <input type="email" className="form-control" id="email" placeholder="Enter email" value={Email} onChange={onEmailHandler} />
+                                                        <label>Password</label>
+                                                        <input type="password" className="form-control" id="password" placeholder="Password" value={Password} onChange={onPasswordHandler} />
+                                                        <br />
+                                                        <Col md="12">
+                                                            <button type="submit"  className="btn btn-success waves-effect waves-light m-r-10">
+                                                                로그인
+                                                            </button>                                                        
+                                                        </Col>
+                                                    </FormGroup>   
+                                                    <div className="col-md-6">
+                                                        <h4 style={{margin:'60px 0'}} className="">회원이 아니신가요?</h4>
+                                                        <Link to="/register">   
+                                                            <button type="submit"  className="btn btn-success waves-effect waves-light m-r-10">
+                                                            회원가입 하기                                                          
+                                                            </button>   
+                                                        </Link>                                         
+                                                    </div>                                             
+                                                </form>
+                                                
+                                            </Col>
+                                        </Row>
+                                    </Container>
+                            </Col>
+                        </Row>
+                    </Container>
+            </div>
+        </div>
+            
     )
 }
 
