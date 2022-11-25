20ab989fb0d0cde3b0cd32ba5468bc4881495122
송영창 <sych7389@dgu.ac.kr>
2022-11-25 18:57:38 +0900

    [refactor] 일정 스키마 변경

--- backend/models/Schedule.js
+++ backend/models/Schedule.js
@@ -1,53 +1,93 @@
 // 유저의 비행 일정 담을 database
 
+const { DateTime } = require('luxon');
 const mongoose = require('mongoose')
-
 const { User } = require('./User');
 
 var scheSchema = mongoose.Schema({
-    "ScheduleName": {
+    ScheduleName: {
         type: String,
         default: "MySchedule",
-        required: true
-    },
-    "Flight_No" : { // iata code
-        type: String,
         required: true
     },
     "author": {
         type: String,
     },
-    "departure":{
-        type: String,
+
+    // fa_flight_id: flightaware 고유 id 위치 지도 가져올 때 필요
+    // cancelled: 비행 취소 여부
+    // status: 항공기 상태, 예정/지연 등등으로 출력됨
+    // gate_origin: 출발지 게이트 번호, gate_destination: 도착지 게이트 번호
+    flight_info: {
+        flight_iata: {
+            type: String,
+        },
+        fa_flight_id: {
+            type: String
+        },
+        cancelled: {
+            type: Boolean
+        },
+        gate_origin: {
+            type: String
+        },
+        gate_destination: {
+            type: String
+        },
+        progress_percent: {
+            type: Number
+        }
     },
-    "dep_iata":{
-        type: String,
-    },
-    "arrival":{
-        type: String,
-    },
-    "arr_iata":{
-        type: String,
-    },
+
+    // scheduled_off, estimated_off, actual_off: 예정/예상/실제 탑승 완료 시간, 비행 시간은 아님
+    // scheduled_on, estimated_on, actual_on: 예정/예상/실제 도착 시간
     // 시간 형식은 ISO 8601, YYYY-MM-DD T HH:MM:SS
-    "dep_time_kr":{
-        type: String,
+    // delay는 항공기 지연되었을 경우 지연된 시간, 초 단위로 받아옴
+    flight_schedule: {
+        scheduled_out: {
+            type: String
+        },
+        estimated_out: {
+            type: String
+        },
+        actual_out: {
+            type: String
+        },
+        scheduled_in: {
+            type: String
+        },
+        estimated_in: {
+            type: String
+        },
+        actual_in: {
+            type: String
+        },
+        delay: {
+            departure_delay: {
+                type: Number
+            },
+            arrival_delay: {
+                type: Number
+            }
+        },
     },
-    "dep_time_utc":{
-        type: String
-    }
 
-    // IATA 형식으로 입력
-    // "From" : String,
-    // "To": String,
-    // "Dep_Time": Date,
-    // "Gate": String,
-    // 나머지는 api 이용해서 받아옴
+    airports: {
+        departure: {
+            type: String,
+        },
+        dep_iata: {
+            type: String,
+        },
+        arrival: {
+            type: String,
+        },
+        arr_iata: {
+            type: String,
+        },
+    },
+
 })
-
-
-
-
 
 
 const Schedule = mongoose.model('Schedule', scheSchema)
