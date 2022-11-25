2a7013ec72ed66983406e346ed94193cad71c4f1
송영창 <sych7389@dgu.ac.kr>
2022-11-25 19:46:53 +0900

    [refactor] 사용하지 않는 코드 삭제

--- backend/index.js
+++ backend/index.js
@@ -8,16 +8,10 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 //비밀키를 가져올 변수
 const config = require('./config/dev');
-//AirLabsAPI 호출 함수
-const session = require('express-session')
 // session 세팅
-const { callAirLabs } = require("./middleware/airlabs");
-//AirLabsAPI 호출에 사용되는 매개변수
-const params = {
-  api_key: config.api_key,
-  //항공기 위치나 다른 API도 찾아오려면 다른 식으로 변경 필요
-  _fields: "dep_iata, dep_time, arr_iata, arr_time"
-}
+const session = require('express-session')
+
+
 
 const usersRouter = require('./routes/users')
 const myPageRouter = require('./routes/myPage')
@@ -60,24 +54,6 @@ app.get('/', (req, res) => {
     res.send('로그인해주세용')
   }
   // res.send('백이 연결되었습니다')
-})
-
-//사용자에게서 항공편명, 출발지를 입력받아 api로 비행일정 검색
-//지역이나 공항이름을 입력받아 iata 코드로 변환하고자 하지만 우선 iata코드 입력하도록 구현
-app.post('/api/schedules/find', (req, res) => {
-  let flight_iata = req.body.flight_iata;
-  let dep_iata = req.body.dep_iata;
-
-  callAirLabs('schedules', flight_iata, dep_iata, params, (err, result) => {
-    if (err) return res.status(400).send(err);
-    //airlabs에서 받아온 결과 중 response 부분만 뽑아 json으로 변환
-    let result_json = JSON.parse(result.body).response;
-    //검색된 결과가 하나도 없는 경우
-    if(result_json.length==0) return res.send("해당 비행일정이 조회되지 않습니다.");
-
-    //요청에 대한 응답으로 결과 json 보내줌
-    return res.status(200).send(result_json);
-  })
 })
 
 app.use('/api/users', usersRouter)
