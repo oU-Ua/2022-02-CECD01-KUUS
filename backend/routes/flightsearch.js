d52277b4d407f368d419572e4fd45de3cbb00bf1
송영창 <sych7389@dgu.ac.kr>
2022-11-25 18:59:50 +0900

    [feat, refactor] flightaware api 사용 추가,  데이터 처리 변경

--- backend/routes/flightsearch.js
+++ backend/routes/flightsearch.js
@@ -3,12 +3,13 @@ const router = express.Router()
 const bodyParser = require("body-parser");
 const { DateTime } = require('luxon')
 const { Airport } = require('../models/Airports');
+const { getFlight, refineResult } = require('../middleware/flightaware')
 router.use(bodyParser.json());
 
 // req 입력값: departure, arrival, flight_iata, date(YYYY-MM-DD), time(HH:MM)
 router.get('/', (req, res) => {
-    Airport.findByNames(req.body.departure, req.body.arrival, (err, results)=>{
-        if(err){
+    Airport.findByNames(req.body.departure, req.body.arrival, (err, results) => {
+        if (err) {
             res.status(400).send(err)
             return;
         }
@@ -16,34 +17,57 @@ router.get('/', (req, res) => {
             //console.log(results);
             // 사용자에게 입력받은 출발일, 출발시간 처리부분
             // flightaware에서 요구하는 형식으로 변환해 저장
-            var daytime = DateTime.fromISO(req.body.date + "T" + req.body.time+":00");
+            var daytime = DateTime.fromISO(req.body.date + "T" + req.body.time + ":00");
             daytime.numberingSystem
-            var time_kr = daytime.toISO({suppressMilliseconds: true})
-            var time_utc = daytime.toUTC().toISO({suppressMilliseconds: true})
+            var time_kr = daytime.toISO({ suppressMilliseconds: true })
+            var time_utc = daytime.toUTC().toISO({ suppressMilliseconds: true })
             // 출력 및 db 저장을 위한 json 객체틀
-            var json = {
+            var airports = {
                 departure: "",
-                dep_iata:"",
+                dep_iata: "",
+                arrival: "",
+                arr_iata: "",
+            }
+            var flight_schedule = {
                 dep_time_utc: time_utc,
-                dep_time_kr: time_kr,
-                arrival:"",
-                arr_iata:"",
-                Flight_No: req.body.flight_iata
+            }
+            var flight_info = {
+                flight_iata: "",
             }
             results.forEach(result => {
-                //console.log("result " + result)
                 // 검색한 지역명이 검색 결과의 공항명에 포함되는지에 따라 출발지와 도착지 구분해 저장
-                if(result.airport_name.includes(req.body.departure)){
-                    json.departure = result.airport_name
-                    json.dep_iata = result.iata_code
+                if (result.airport_name.includes(req.body.departure)) {
+                    airports.departure = result.airport_name
+                    airports.dep_iata = result.iata_code
                 }
-                else{
-                    json.arrival = result.airport_name
-                    json.arr_iata = result.iata_code
+                else {
+                    airports.arrival = result.airport_name
+                    airports.arr_iata = result.iata_code
                 }
+
             });
-            res.status(200).send(json)
         }
+        var flight_iata = req.body.flight_iata
+        // flightaware api 통해 검색
+        getFlight(flight_iata, flight_schedule.dep_time_utc, (err, result) => {
+            if (err) {
+                res.status(400).send(err)
+                return;
+            }
+            else {
+                var fa_result = JSON.parse(result.body).flights[0]
+                // console.log(fa_result)
+                // 받아온 정보 처리
+                refineResult(fa_result, (info, schedule) => {
+                    flight_info = info
+                    flight_info.flight_iata = flight_iata
+                    flight_schedule = schedule
+                    flight_schedule.dep_time_kr = time_kr
+                })
+            }
+            // 이걸 가지고 일정 등록 수행
+            res.status(200).send({ airports, flight_info, flight_schedule })
+        })
     })
 })
 
