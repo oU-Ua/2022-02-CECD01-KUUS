293f322e528578a02b9765a9a81792f10e7e4951
yujjineee <skekskekskek@gmail.com>
2022-11-26 20:21:54 +0900

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -10,3 +10,5 @@ module.exports = function(app) {
     })
   );
 };
+
+// https://velog.io/@dovnaldisn/React-Router-Dom-Flow-Axios-CORS%EC%9D%B4%EC%8A%88-Proxy-%EC%84%A4%EC%A0%95-Concurrently-Redux-%EA%B8%B0%EC%B4%88-2026
