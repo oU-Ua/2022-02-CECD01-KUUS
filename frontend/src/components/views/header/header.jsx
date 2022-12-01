c1234ead2e8d18c97fba461994ed1f53eaae1104
KimKongDol <als6213@naver.com>
2022-12-01 14:05:00 +0900

    [fix]헤더 버튼 고치기

--- frontend/src/components/views/header/header.jsx
+++ frontend/src/components/views/header/header.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable */
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 // import { HashLink as Link } from 'react-router-hash-link';
+import { useDispatch } from 'react-redux';
 import { Container, NavbarBrand, Navbar, Nav, NavItem, NavbarToggler, Collapse } from 'reactstrap';
 import {Link} from 'react-router-dom'
 import logo from '../../../assets/images/logos/kuus-white-text.png';
+import { auth } from '../../../_actions/user_action';
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -16,6 +18,34 @@ const Header = () => {
     const onClickHandler = () => {
         history.push("/mypage")
     }
+
+    // function AuthenticationCheck(props) {
+    //     const dispatch = useDispatch();
+
+    //     useEffect(() => {
+    //         dispatch(auth()).then(response => {
+    //             console.log('front의 auth입니다! response.payload: ')
+    //             console.log(response.payload)
+    //             //로그인 하지 않은 상태 
+    //             if (!response.payload.isAuth) {
+    //                 if (option) {
+    //                     props.history.push('/login')
+    //                 }
+    //             } else {
+    //                 //로그인 한 상태 
+    //                 if (adminRoute && !response.payload.isAdmin) {
+    //                     props.history.push('/')
+    //                 } else {
+    //                     if (option === false)
+    //                         props.history.push('/')
+    //                 }
+    //             }
+    //         })
+    //     }, [])
+
+    //     return ( )
+    // }
+
 
     return (
         <div className="topbar" id="top">
