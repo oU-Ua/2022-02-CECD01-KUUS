a8c4f58c55f00f2fdfe2d0527cffc00652b01f03
yujjineee <skekskekskek@gmail.com>
2022-12-01 18:35:16 +0900

    [feat] mypage 내 일정 공유 일정 분리 출력

--- frontend/src/components/views/MyPage/MyPage.js
+++ frontend/src/components/views/MyPage/MyPage.js
@@ -5,6 +5,8 @@ import { Container, Row, Col, Button, Card, CardTitle, CardText } from 'reactstr
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { auth } from '../../../_actions/user_action';
+import { scheduleJob } from 'node-schedule';
+import MyPageSchedule from './MyPageSchedule';
 
 // 무한반복 해결하기 
 // https://sir.kr/qa/422561
@@ -14,15 +16,14 @@ function MyPage({ match }) {
     const dispatch = useDispatch();
 
     const [users, setUsers] = useState([]);
-    const [scheName, setScheName] = useState([])
-    // const [scheAuthor, setScheAuthor] = useState([])
+    const [mySchedule, setMySchedule] = useState([])
+    const [sharedSchedule, setSharedSchedule] = useState([])
 
     function detailClick(e) {
         window.location.href = 'http://localhost:3000/mypage/schedules/638759c936462573ed5c6e23'
     }
 
     async function getUsers() {
-        console.log('func 진입')
         dispatch(auth())
             .then(response =>
                 axios.get('http://localhost:5000/api/mypage', {
@@ -33,10 +34,12 @@ function MyPage({ match }) {
                         // console.log(response)
                         setUsers(response.data)
                         // console.log('front MyPage.js입니다 response.data:');
-                        // console.log(response.data)
-                        const myschedules = response.data["myschedules"]
-                        setScheName(myschedules[0].ScheduleName)
+                        console.log(response.data)
+                        // const myschedules = response.data["myschedules"]
+                        setMySchedule(response.data["myschedules"][0])
                         // setScheAuthor(response.data["myschedules"][0].author)
+                        setSharedSchedule(response.data["sharedschedules"][0])
+
                     }).catch(function (error) {
                         console.error(error)
                     })
@@ -47,18 +50,6 @@ function MyPage({ match }) {
     useEffect(function () {
         getUsers()
     }, [])
-
-    // console.log('users 아래에 띄움')
-    // console.log(users)
-
-    // const usersKey = Object.keys(users)
-    // console.log('usersKey')
-    // console.log(usersKey)
-
-    console.log('scheName: ' + scheName)
-
-
-
 
     return (
         <div>
@@ -77,7 +68,7 @@ function MyPage({ match }) {
                         <Row className="justify-content-center">
                             <Col md="7" className="text-center">
                                 <h2 className="title font-bold">
-                                    {users.name}님 환영합니다.</h2>
+                                    {users.name}님 환영합니다 !</h2>
                                 <h6 className="subtitle">{users.email}</h6>
                             </Col>
                         </Row>
@@ -88,16 +79,16 @@ function MyPage({ match }) {
                         <Col md="6">
                             <h3 className="title font-bold text-center">나의 일정</h3>
                             <Card body className="card-shadow">
-                                <CardTitle>일정 1</CardTitle>
-                                <CardText className='font-bold display-7'>{scheName}</CardText>
+                                <CardTitle className='font-bold display-7'>{mySchedule.ScheduleName}</CardTitle>
+                                <CardText></CardText>
                                 <Button onClick = {detailClick}>자세히 보기</Button>
                             </Card>
                         </Col>
                         <Col md="6">
                             <h3 className="title font-bold text-center">공유받은 일정</h3>
                             <Card body className="card-shadow">
-                                <CardTitle>일정2</CardTitle>
-                                <CardText className='font-bold display-7'>{scheName}</CardText>
+                            <CardTitle className='font-bold display-7'>{sharedSchedule.ScheduleName}</CardTitle>
+                                <CardText></CardText>
                                 <Button onClick={detailClick}>자세히 보기</Button>
                             </Card>
                         </Col>
